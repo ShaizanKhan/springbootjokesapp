8e837401c3b53c0c66955d499ed08eea3dd4aef0
Shaizan Khan <shaizan.khan@betsol.com>
2019-11-19 22:59:31 +0530

    Using XML based configuration for beans.

--- src/main/java/com/shaizankhan/springbootjokesapp/SpringbootjokesappApplication.java
+++ src/main/java/com/shaizankhan/springbootjokesapp/SpringbootjokesappApplication.java
@@ -2,8 +2,10 @@ package com.shaizankhan.springbootjokesapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource("classpath:chuck-config.xml")
 public class SpringbootjokesappApplication {
 
     public static void main(String[] args) {
